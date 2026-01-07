7cc9639f959784237e88bc4a2e79271e11a1c904
ifit.tw01 <ifit.tw01@gmail.com>
2026-01-07 10:13:12 +0800

    ?湔 LINE ?????Google Sheet ID

--- google-apps-script-simple.js
+++ google-apps-script-simple.js
@@ -7,7 +7,7 @@
 // ========================================
 // 配置设置
 // ========================================
-const SPREADSHEET_ID = '1izx4MEi7coasKaULlqLQUDOmXt9qEmQ8GC0ZKfT2XvU';  // 你的 Google Sheet ID
+const SPREADSHEET_ID = '1cmLE1clkzQKeqdtLa-DS9JfSEg3wI6hr1egHRujdPiQ';  // 你的 Google Sheet ID
 const SHEET_NAME_PROMOTERS = '推廣人員';  // 推广人员工作表
 const SHEET_NAME_REGIONS = '評估地點';    // 评估地点工作表
 const SHEET_NAME_CUSTOMERS = '客戶報名記錄';  // 客户报名记录工作表
